bfbbdbe018900e0817203611db75a7d78da0799a
Danique Wijnalda <danique@van-ons.nl>
2026-06-15 14:52:25 +0200

    style: sort php-cs-fixer rules

--- .php_cs.dist.php
+++ .php_cs.dist.php
@@ -14,18 +14,10 @@ return (new PhpCsFixer\Config())
     ->setRules([
         '@PSR12' => true,
         'array_syntax' => ['syntax' => 'short'],
-        'ordered_imports' => ['sort_algorithm' => 'alpha'],
-        'no_unused_imports' => true,
-        'not_operator_with_successor_space' => true,
-        'trailing_comma_in_multiline' => true,
-        'phpdoc_scalar' => true,
-        'unary_operator_spaces' => true,
         'binary_operator_spaces' => true,
         'blank_line_before_statement' => [
             'statements' => ['break', 'continue', 'declare', 'return', 'throw', 'try'],
         ],
-        'phpdoc_single_line_var_spacing' => true,
-        'phpdoc_var_without_name' => true,
         'class_attributes_separation' => [
             'elements' => [
                 'method' => 'one',
@@ -35,6 +27,16 @@ return (new PhpCsFixer\Config())
             'on_multiline' => 'ensure_fully_multiline',
             'keep_multiple_spaces_after_comma' => true,
         ],
+        'no_unused_imports' => true,
+        'not_operator_with_successor_space' => true,
+        'ordered_imports' => [
+            'sort_algorithm' => 'alpha',
+        ],
+        'phpdoc_scalar' => true,
+        'phpdoc_single_line_var_spacing' => true,
+        'phpdoc_var_without_name' => true,
         'single_trait_insert_per_statement' => true,
+        'trailing_comma_in_multiline' => true,
+        'unary_operator_spaces' => true,
     ])
     ->setFinder($finder);
